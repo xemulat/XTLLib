8f4b8d712573d38069c4c3bb513588f64cd34d93
xemulat <98595166+xemulat@users.noreply.github.com>
2022-11-07 22:10:14 +0100

    Add files via upload

--- XTLLib/runqol.py
+++ XTLLib/runqol.py
@@ -1,4 +1,4 @@
 def runqol(froms, choose):
     if choose == "99": exit()
-    if froms != 0 and achooser == "h" or choose == "H": helpe(froms)
+    if froms != 0 and (choose == "h" or choose == "H"): helpe(froms)
     else: print("No option named " + choose); sleep(3)
